3a35957540636a6fc04d5cc0c422add43702a363
rockske8er <rockske8er@gmail.com>
2022-11-30 10:52:22 -0300

    fix: package-lock remove

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import { Header } from '../components/Header'
-import { Characters } from '../layouts/Characters'
-import { Game } from '../layouts/Game'
-import { Hero } from '../layouts/Hero'
-import { Story } from '../layouts/Story'
+// import { Characters } from '../layouts/Characters'
+// import { Game } from '../layouts/Game'
+// import { Hero } from '../layouts/Hero'
+// import { Story } from '../layouts/Story'
 
 export default function Home() {
   return (
